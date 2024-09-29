e7c358195de08b6ed17f301efcd0052ea503d99b
Tomasz Dudziak <tomasz.dudziak@gmail.com>
2024-09-29 20:50:14 +0200

    Fix sizing bug in initFrambuffer()

--- main.js
+++ main.js
@@ -274,7 +274,7 @@ const onLoad = showErrorOnFailure(function() {
         const framebuffer = gl.createFramebuffer();
         const texture = gl.createTexture();
         gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA8, width, width, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA8, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
